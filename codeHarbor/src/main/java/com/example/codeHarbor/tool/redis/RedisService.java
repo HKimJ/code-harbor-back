11b51e3495550e53cf533edafe4769c6ba55d23f
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-16 22:39:53 +0900

    레디스_서비스_추가
    테스트 필요

---
+++ codeHarbor/src/main/java/com/example/codeHarbor/tool/redis/RedisService.java
@@ -0,0 +1,23 @@
+package com.example.codeHarbor.tool.redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+    protected final RedisTemplate<String, String> redisTemplate;
+
+    public RedisService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void storeDataInRedis(String email, String verifyCode, int expireTime) {
+        redisTemplate.opsForValue().set(email, verifyCode, expireTime, TimeUnit.MINUTES);
+    }
+
+    public String retrieveDataFromRedis(String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+}
