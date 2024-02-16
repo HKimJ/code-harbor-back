89ce0aa4b719ef9dc0e475f899b7293abc016fdc
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-16 22:44:46 +0900

    메일전송및레디스기능추가
    테스트필요

--- codeHarbor/src/main/java/com/example/codeHarbor/user/service/UserCrudService.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/service/UserCrudService.java
@@ -1,5 +1,6 @@
 package com.example.codeHarbor.user.service;
 
+import com.example.codeHarbor.tool.javamail.JavaMailService;
 import com.example.codeHarbor.user.domain.UserDomain;
 import com.example.codeHarbor.user.dto.UserCrudRequestDto;
 import com.example.codeHarbor.user.dto.UserCrudResponseDto;
@@ -11,27 +12,78 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserCrudService {
     private final UserRepository userRepo;
+    private final JavaMailService mailSender;
+    public UserCrudResponseDto checkId(UserCrudRequestDto input) {
+        String id = input.getUserId();
+        UserCrudResponseDto response = new UserCrudResponseDto();
+        try {
+            if (userRepo.existsByUserId(id)) {
+                response.setSuccess(false);
+                response.setData("이미 가입된 이메일입니다.");
+            } else {
+                mailSender.sendVerificationMail(id);
+                response.setSuccess(true);
+                response.setData("인증 메일을 발송했습니다.");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            response.setSuccess(false);
+            response.setData("중복 체크시 문제가 발생했습니다. 관리자에게 문의해주세요.");
+        }
+        return response;
+    }
 
-//    public UserCrudResponseDto createUser(UserCrudRequestDto input) {
-//        UserCrudResponseDto response = new UserCrudResponseDto();
-//        String id = input.getUserId();
-//        String pw = input.getUserPw();
-//        try {
-//            UserDomain isNewUser = userRepo.findUserByUserId(id);
-//            if (isNewUser != null) {
-//                UserDomain checkUser = userRepo.findUserByUserIdAndUserPassword(id, pw);
-//                if (dbUser.equals(checkUser)) {
-//                    response.setSuccess(true);
-//
-//                } else {
-//
-//                }
-//            } else {
-//                response.setSuccess(false);
-//                response.setData("아이디 혹은 비밀번호가 일치하지 않습니다.");
-//            }
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public UserCrudResponseDto verifyCode(UserCrudRequestDto input) {
+        UserCrudResponseDto response = new UserCrudResponseDto();
+        try {
+            response = mailSender.confirmVerificationMail(input.getUserId(), input.getVerifyCode());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setSuccess(false);
+            response.setData("인증코드 검증과정에서 문제가 발생했습니다.");
+        }
+        return response;
+    }
+
+    public UserCrudResponseDto checkExistNick(UserCrudRequestDto input) {
+        String nick = input.getUserNick();
+        UserCrudResponseDto response = new UserCrudResponseDto();
+        try {
+            if (userRepo.existsByUserNick(nick)) {
+                response.setSuccess(false);
+                response.setData("중복된 닉네임입니다.");
+            } else {
+                response.setSuccess(true);
+                response.setData("가입 가능한 닉네임입니다.");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            response.setSuccess(false);
+            response.setData("문제가 발생했습니다. 관리자에게 문의해주세요.");
+        }
+        return response;
+    }
+    public UserCrudResponseDto basicUserCrud(UserCrudRequestDto input) {
+        UserCrudResponseDto response = new UserCrudResponseDto();
+        String id = input.getUserId();
+        String nick = input.getUserNick();
+        String pw = input.getUserPw();
+        try {
+            UserDomain newUser = new UserDomain();
+            newUser.setUserId(id);
+            newUser.setUserNick(nick);
+            newUser.setUserPassword(pw);
+            userRepo.save(newUser);
+
+            response.setSuccess(true);
+            response.setData("회원 가입에 성공했습니다.");
+        } catch(Exception e) {
+            e.printStackTrace();
+            response.setSuccess(false);
+            response.setData("회원가입 중 문제가 발생했습니다. 관리자에게 문의하세요");
+
+        }
+        return response;
+    }
 }
