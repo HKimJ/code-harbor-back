3be47e70163e843b26e370c7512aa245402a309b
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-17 02:20:41 +0900

    swagger 정리

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
@@ -1,18 +1,22 @@
 package com.example.codeHarbor.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.jetbrains.annotations.Nullable;
 
 @Data
 public class UserCrudRequestDto {
-    @Email
-    @Nullable
+    @Email @Nullable
+    @Schema(description = "클라이언트에서 입력한 메일 형태의 이용자 ID", example = "test@naver.com")
     private String userId;
     @Nullable
-    private String userNick;
+    @Schema(description = "클라이언트에서 입력한 이용자 닉네임", example = "tester1")
+    private String userNickname;
     @Nullable
-    private String userPw;
+    @Schema(description = "클라이언트에서 입력한 이용자 비밀번호", example = "testPw")
+    private String userPassword;
     @Nullable
+    @Schema(description = "클라이언트에서 입력한 이메일 인증코드", example = "1234")
     private String verifyCode;
 }
