641dd389c78ef112c5b0ddee45eb8603f7fe2605
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-17 02:20:57 +0900

    swagger 정리

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserLoginRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserLoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.codeHarbor.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,7 +12,8 @@ import lombok.NoArgsConstructor;
 public class UserLoginRequestDto {
     @NotNull
     @Email
+    @Schema(description="로그인시 사용자가 입력한 아이디", example = "test@naver.com")
     private String userId;
-
+    @Schema(description="로그인시 사용자가 입력한 비밀번호", example = "test1234")
     private String userPassword; // SNS 로그인에서 불필요
 }
