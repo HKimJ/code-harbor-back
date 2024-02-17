a91b589ca7502a0216d440b6ad5714061892f8e1
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-17 16:40:21 +0900

    dto 구조수정

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudResponseDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudResponseDto.java
@@ -2,8 +2,10 @@ package com.example.codeHarbor.user.dto;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class UserCrudResponseDto {
     private boolean success;
-    private String data;
+    private Map<String, Object> data;
 }
