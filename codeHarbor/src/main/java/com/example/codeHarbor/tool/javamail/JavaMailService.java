644a3092153bf23bb30692bd4fb35afa208c2967
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-17 16:35:33 +0900

    비밀번호 찾기 기능추가 및 responseDto_구조수정
    boolean success, Map<String, Object> data 형태로 전송

--- codeHarbor/src/main/java/com/example/codeHarbor/tool/javamail/JavaMailService.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/tool/javamail/JavaMailService.java
@@ -8,6 +8,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Service
@@ -41,23 +43,55 @@ public class JavaMailService {
 
     public UserCrudResponseDto confirmVerificationMail(String email, String verifyCode) {
         UserCrudResponseDto response = new UserCrudResponseDto();
+        Map<String, Object> data = new HashMap<>();
         try {
             String storedCode = redisService.retrieveDataFromRedis(email);
             if (storedCode.equals(verifyCode)) {
                 response.setSuccess(true);
-                response.setData("인증 성공");
+                data.put("msg", "인증 코드가 일치합니다.");
+                response.setData(data);
             } else {
                 response.setSuccess(false);
-                response.setData("인증코드가 일치하지 않습니다.");
+                data.put("msg", "인증 코드가 일치하지 않습니다.");
+                response.setData(data);
             }
             return response;
         } catch (Exception e) {
             e.printStackTrace();
             response.setSuccess(false);
-            response.setData("인증 코드가 만료되었거나 인증 과정에서 문제가 발생했습니다.");
+            data.put("msg", "인증 코드가 만료되었거나 인증 과정에서 문제가 발생했습니다.");
+            response.setData(data);
             return response;
         }
     }
+
+    public UserCrudResponseDto sendLostPasswordMail(String email, String password) {
+        UserCrudResponseDto response = new UserCrudResponseDto();
+        Map<String, Object> data = new HashMap<>();
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        String title = "[CodeHarbor 비밀번호]";
+        StringBuilder content = new StringBuilder();
+        content.append("<h3> 기존 비밀번호는 다음과 같습니다.</h3>")
+                .append("<h1> [ ").append(password).append(" ]</h1>");
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(email); // 메일 수신자
+            mimeMessageHelper.setSubject(title); // 메일 제목
+            mimeMessageHelper.setText(content.toString(), true); // 메일 본문 내용, HTML 여부
+            mailSender.send(mimeMessage);
+            response.setSuccess(true);
+            data.put("msg", "비밀번호 찾기 메일이 발송되었습니다.");
+            response.setData(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setSuccess(false);
+            data.put("msg", "비밀번호 찾기 메일 발송에 실패했습니다. 문제 지속시 관리자에게 연락해주세요.");
+            response.setData(data);
+        }
+        return response;
+    }
+
+
     private String generateCode() {
         Random random = new Random();
         int code = random.nextInt(9000) + 1000; // Generates a 4-digit number between 1000 and 9999
