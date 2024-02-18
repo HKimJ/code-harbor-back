26ded252c539589564cab21f35d424551784ab49
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-18 16:40:50 +0900

    리펙터링
    Adviser와 연동하기 위해 @Builder 적용하고 셋팅만 해둠

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
@@ -1,15 +1,17 @@
 package com.example.codeHarbor.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
 import org.jetbrains.annotations.Nullable;
 
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@ToString
+@Builder
+@Jacksonized
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCrudRequestDto {
@@ -25,4 +27,21 @@ public class UserCrudRequestDto {
     @Nullable
     @Schema(description = "클라이언트에서 입력한 이메일 인증코드", example = "1234")
     private String verifyCode;
+
+
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//    public void setUserNickname(String userNickname) {
+//        this.userNickname = userNickname;
+//    }
+//    public void setUserPassword(String userPassword) {
+//        this.userPassword = userPassword;
+//    }
+//    public void setVerifyCode(String verifyCode) {
+//        this.verifyCode = verifyCode;
+//    }
+
+
+
 }
