1b6ec23138d7867cc3f3e41fc38577b79818fe34
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-22 17:36:52 +0900

    엔티티_생성
    그룹 엔티티 생성

---
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/domain/GroupDomain.java
@@ -0,0 +1,40 @@
+package com.example.codeHarbor.group.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name="group_info")
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDomain {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupSeq;
+    @Column(columnDefinition = "VARCHAR(50)", updatable = false)
+    private String groupCreator;
+    @Column(columnDefinition = "VARCHAR(30)", unique = true)
+    private String groupName;
+//    @Column(columnDefinition = "json")
+//    private HashSet<String> groupMember;
+    @Temporal(value = TemporalType.DATE) @Column(columnDefinition = "DATE DEFAULT (CURRENT_DATE)", insertable = false, updatable = false)
+    private Date groupCreatedate;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        GroupDomain group = (GroupDomain) obj;
+        return Objects.equals(groupSeq, group.groupSeq);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupSeq);
+    }
+
+}
--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserLoginResponseDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserLoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.codeHarbor.user.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -18,9 +19,8 @@ public class UserLoginResponseDto {
     private boolean success;
 
     @Schema(description = "로그인 성공시 key, value로 유저 데이터 전송<br>" +
-            "로그인 실패시 실패 메시지 전달", example = "data.id: test@naver.com<br/>" +
-            " data.nickname: test<br/>" +
-            "data.msg: 아이디나 비밀번호가 일치하지 않습니다.")
+            "로그인 실패시 실패 메시지 전달",
+            example = "data.userId: test@naver.com " + "data.userNickname: test " + "data.msg: 아이디나 비밀번호가 일치하지 않습니다.")
     @NotNull
     private Map<String, Object> data;
 
