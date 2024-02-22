4e88cf9167deffc3bc27428453dcf54ccf58e7f5
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-22 17:38:50 +0900

    불필요 부분 제거

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudRequestDto.java
@@ -1,7 +1,5 @@
 package com.example.codeHarbor.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import lombok.*;
@@ -16,7 +14,7 @@ import org.jetbrains.annotations.Nullable;
 @AllArgsConstructor
 public class UserCrudRequestDto {
     @Email @Nullable
-    @Schema(name="userId", description = "클라이언트에서 입력한 메일 형태의 이용자 ID", example = "test@naver.com")
+    @Schema(description = "클라이언트에서 입력한 메일 형태의 이용자 ID", example = "test@naver.com")
     private String userId;
     @Nullable
     @Schema(description = "클라이언트에서 입력한 이용자 닉네임", example = "tester1")
