1be33644359166587f007748960f6e3a989f07d8
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 15:00:27 +0900

    임시코드
    추후 삭제예정

--- codeHarbor/src/main/java/com/example/codeHarbor/tool/redis/RedisService.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/tool/redis/RedisService.java
@@ -3,6 +3,7 @@ package com.example.codeHarbor.tool.redis;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -19,5 +20,11 @@ public class RedisService {
 
     public String retrieveDataFromRedis(String email) {
         return redisTemplate.opsForValue().get(email);
+    }
+
+    public String generateCode(String fragment) {
+        Random random = new Random();
+        int code = random.nextInt(9000) + 1000; // Generates a 4-digit number between 1000 and 9999
+        return String.valueOf(fragment + code);
     }
 }
