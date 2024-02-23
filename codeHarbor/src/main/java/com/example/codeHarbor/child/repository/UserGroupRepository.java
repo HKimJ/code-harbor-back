194bbbca06933108a17012409a7a5426646dee69
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 14:56:30 +0900

    중간_레포지토리추가
    UserGroupRepository 추가

---
+++ codeHarbor/src/main/java/com/example/codeHarbor/child/repository/UserGroupRepository.java
@@ -0,0 +1,15 @@
+package com.example.codeHarbor.child.repository;
+
+import com.example.codeHarbor.child.domain.UserGroupDomain;
+import com.example.codeHarbor.group.domain.GroupDomain;
+import com.example.codeHarbor.user.domain.UserDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserGroupRepository extends JpaRepository<UserGroupDomain, Long> {
+    UserGroupDomain findUserGroupByUserAndGroup(UserDomain user, GroupDomain group);
+    boolean existsByUserAndGroup(UserDomain user, GroupDomain group);
+
+    UserGroupDomain save(UserDomain user, GroupDomain group);
+}
