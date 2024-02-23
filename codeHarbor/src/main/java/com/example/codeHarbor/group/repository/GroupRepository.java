0ff064537fd40b330caa57056c1f74ba633e8a96
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 14:59:10 +0900

    불필요필드_삭제

--- codeHarbor/src/main/java/com/example/codeHarbor/group/repository/GroupRepository.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/repository/GroupRepository.java
@@ -4,13 +4,11 @@ import com.example.codeHarbor.group.domain.GroupDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface GroupRepository extends JpaRepository<GroupDomain, Long> {
     GroupDomain findGroupByGroupCreator(String groupCreator);
+    GroupDomain findGroupByGroupName(String groupName);
     boolean existsByGroupName(String groupName);
-//    List<GroupDomain> findBySerialzable
     GroupDomain save(GroupDomain groupDomain);
 
 }
