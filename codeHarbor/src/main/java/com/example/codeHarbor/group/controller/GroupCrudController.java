3d98af37869643aa3988268048f1f6d6ea44c090
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 19:42:18 +0900

    기능추가
    그룹 생성시 중복체크 기능 추가

--- codeHarbor/src/main/java/com/example/codeHarbor/group/controller/GroupCrudController.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/controller/GroupCrudController.java
@@ -3,7 +3,6 @@ package com.example.codeHarbor.group.controller;
 import com.example.codeHarbor.group.dto.GroupCrudRequestDto;
 import com.example.codeHarbor.group.dto.GroupCrudResponseDto;
 import com.example.codeHarbor.group.service.GroupCrudService;
-import com.example.codeHarbor.user.dto.UserCrudResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -26,6 +25,34 @@ import java.util.Map;
 public class GroupCrudController {
 
     private final GroupCrudService crudService;
+
+    @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
+            examples = {
+                    @ExampleObject(name = "Example", value = """ 
+                { 
+                    "groupName" : "myGroup" 
+                } 
+            """)}))
+    @PostMapping(value = "/checkGroupName", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "그룹 생성자가 입력한 정보를 바탕으로 그룹생성", description = "그룹명 전달받아 중복체크")
+    public ResponseEntity<GroupCrudResponseDto> checkGroupName(@Valid @RequestBody GroupCrudRequestDto input, BindingResult bindingResult)
+    {
+        System.out.println("그룹 생성시 그룹이름 중복체크 시도");
+        if (bindingResult.hasErrors()) {
+            Map<String, Object> data = new HashMap<>();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                String fieldName = error.getField();
+                String errorMsg = error.getDefaultMessage();
+                System.out.println("입력 유효성 검증 실패 - 필드명: " + fieldName + ", 에러메세지: " + errorMsg);
+            }
+            data.put("msg", "올바른 매개변수명이나 형식이 전달되지 않았습니다.");
+            GroupCrudResponseDto response = GroupCrudResponseDto.builder().success(false).data(data).build();
+            return ResponseEntity.ok(response);
+        }
+        GroupCrudResponseDto response = crudService.checkGroupName(input);
+        return ResponseEntity.ok(response);
+    }
     @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
             examples = {
                     @ExampleObject(name = "Example", value = """ 
@@ -90,12 +117,13 @@ public class GroupCrudController {
                     "verify" : "certainValue"(메일 전송시 임의의 값으로 설정되어있음, 프론트는 상관안해도 됨)
                 }
             """)}))
+    @CrossOrigin
     @PostMapping(value = "/acceptExistMember")
-    @Operation(summary = "그룹원이 기존에 서비스에 가입된 인원을 그룹에 초대하는 요청", description = "초대자의 그룹명과 초대할 인원의 이메일(userId)을 전달받아 그룹에 초대")
-    public ResponseEntity<GroupCrudResponseDto> acceptExistUserAsMember(@RequestParam String groupInvitee, @RequestParam String verify) {
+    @Operation(summary = "그룹원이 기존에 서비스에 가입된 인원을 그룹에 초대하는 api", description = "초대자의 그룹명과 초대할 인원의 이메일(userId)을 전달받아 그룹에 초대")
+    public void acceptExistUserAsMember(@RequestParam String groupName, @RequestParam String groupInvitee, @RequestParam String groupInviteVerify) {
         System.out.println("그룹원 초대 수락버튼 클릭됨");
-        GroupCrudResponseDto response = crudService.acceptExistUserAsMember(GroupCrudRequestDto.builder().groupInvitee(groupInvitee).groupInviteVerify(verify).build());
-        return ResponseEntity.ok(response);
+        GroupCrudResponseDto response = crudService.acceptExistUserAsMember(GroupCrudRequestDto.builder().groupName(groupName).groupInvitee(groupInvitee).groupInviteVerify(groupInviteVerify).build());
+        System.out.println(response);
     }
 
     @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
