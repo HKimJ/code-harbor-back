d35507b92d093858c3bdde3681e00a20f52da598
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 21:28:28 +0900

    설명 상세화

--- codeHarbor/src/main/java/com/example/codeHarbor/group/dto/GroupCrudRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/dto/GroupCrudRequestDto.java
@@ -8,11 +8,13 @@ import lombok.*;
 @Builder
 
 public class GroupCrudRequestDto {
-    @Schema(description = "그룹생성 요청을 보낸 이용자 id", example = "test@naver.com")
+    @Schema(description = "최초 그룹생성 요청을 보낸 이용자 id", example = "test@naver.com")
     private final String groupCreator;
     @Schema(description = "그룹생성 요청시 전송한 그룹이름", example = "myGroup1")
     private final String groupName;
-    @Schema(description = "그룹에 초대할 인원의 userId(email)", example = "invitee@naver.com")
+    @Schema(description = "그룹원 초대 요청을 보낸 이용자 id", example = "invitor@naver.com")
+    private final String groupInvitor;
+    @Schema(description = "그룹에 초대할 인원의 userId or email 주소)", example = "invitee@naver.com")
     private final String groupInvitee;
     @Schema(description = "그룹에 초대할 인원의 인증용 value(추후 토큰등으로 교체)", example = "certainValue")
     private final String groupInviteVerify;
