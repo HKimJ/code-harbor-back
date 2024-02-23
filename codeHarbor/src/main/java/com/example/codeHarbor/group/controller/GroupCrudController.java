7784604f5f547b98906d0e413e5d223cc14533f6
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 14:57:35 +0900

    기능추가
    그룹에 회원초대 기능, 기존 회원 그룹에 추가, 신규 회원 그룹에 추가 기능

--- codeHarbor/src/main/java/com/example/codeHarbor/group/controller/GroupCrudController.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/controller/GroupCrudController.java
@@ -14,10 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -55,6 +52,64 @@ public class GroupCrudController {
             return ResponseEntity.ok(response);
         }
         GroupCrudResponseDto response = crudService.createGroup(input);
+        return ResponseEntity.ok(response);
+    }
+    @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
+            examples = {
+                    @ExampleObject(name = "Example", value = """ 
+                { 
+                    "groupName" : "myGroup",
+                    "groupInvitee" : "newMember@naver.com" 
+                } 
+            """)}))
+    @PostMapping(value = "/inviteGroup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "그룹원이 새로운 인원을 초대하는 요청", description = "초대자의 그룹명과 초대할 인원의 이메일(userId)을 전달받아 그룹에 초대")
+    public ResponseEntity<GroupCrudResponseDto> inviteGroup(@Valid @RequestBody GroupCrudRequestDto input, BindingResult bindingResult)
+    {
+        System.out.println("그룹원 초대 시도");
+        if (bindingResult.hasErrors()) {
+            Map<String, Object> data = new HashMap<>();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                String fieldName = error.getField();
+                String errorMsg = error.getDefaultMessage();
+                System.out.println("입력 유효성 검증 실패 - 필드명: " + fieldName + ", 에러메세지: " + errorMsg);
+            }
+            data.put("msg", "올바른 매개변수명이나 형식이 전달되지 않았습니다.");
+            GroupCrudResponseDto response = GroupCrudResponseDto.builder().success(false).data(data).build();
+            return ResponseEntity.ok(response);
+        }
+        GroupCrudResponseDto response = crudService.inviteMember(input);
+        return ResponseEntity.ok(response);
+    }
+
+    @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
+            examples = {
+                    @ExampleObject(name = "Example", value = """ 
+                {
+                    "verify" : "certainValue"(메일 전송시 임의의 값으로 설정되어있음, 프론트는 상관안해도 됨)
+                }
+            """)}))
+    @PostMapping(value = "/acceptExistMember")
+    @Operation(summary = "그룹원이 기존에 서비스에 가입된 인원을 그룹에 초대하는 요청", description = "초대자의 그룹명과 초대할 인원의 이메일(userId)을 전달받아 그룹에 초대")
+    public ResponseEntity<GroupCrudResponseDto> acceptExistUserAsMember(@RequestParam String groupInvitee, @RequestParam String verify) {
+        System.out.println("그룹원 초대 수락버튼 클릭됨");
+        GroupCrudResponseDto response = crudService.acceptExistUserAsMember(GroupCrudRequestDto.builder().groupInvitee(groupInvitee).groupInviteVerify(verify).build());
+        return ResponseEntity.ok(response);
+    }
+
+    @io.swagger.v3.oas.annotations.parameters.RequestBody (content = @Content(
+            examples = {
+                    @ExampleObject(name = "Example", value = """ 
+                { 
+                    "verify" : "certainValue"(메일 전송시 임의의 값으로 설정되어있음, 프론트는 상관안해도 됨)
+                } 
+            """)}))
+    @PostMapping(value = "/acceptNewMember")
+    @Operation(summary = "그룹원이 새로 서비스에 가입된 인원을 그룹에 초대하는 요청", description = "초대자의 그룹명과 초대할 인원의 이메일(userId)을 전달받아 그룹에 초대")
+    public ResponseEntity<GroupCrudResponseDto> acceptNewUserAsMember(@RequestParam String groupInvitee, @RequestParam String verify) {
+        System.out.println("그룹원 초대 수락버튼 클릭됨");
+        GroupCrudResponseDto response = crudService.acceptExistUserAsMember(GroupCrudRequestDto.builder().groupInvitee(groupInvitee).groupInviteVerify(verify).build());
         return ResponseEntity.ok(response);
     }
 }
