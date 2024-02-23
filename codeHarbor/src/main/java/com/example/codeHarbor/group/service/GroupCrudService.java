380dfffb358e156fa48962d8158a279857a7d0b6
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-23 14:59:35 +0900

    기능추가
    서비스단에서 그룹초대, 기존회원 수락, 신규회원 수락 로직 추가

--- codeHarbor/src/main/java/com/example/codeHarbor/group/service/GroupCrudService.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/service/GroupCrudService.java
@@ -1,9 +1,13 @@
 package com.example.codeHarbor.group.service;
 
+import com.example.codeHarbor.child.repository.UserGroupRepository;
 import com.example.codeHarbor.group.domain.GroupDomain;
 import com.example.codeHarbor.group.dto.GroupCrudRequestDto;
 import com.example.codeHarbor.group.dto.GroupCrudResponseDto;
 import com.example.codeHarbor.group.repository.GroupRepository;
+import com.example.codeHarbor.tool.javamail.JavaMailService;
+import com.example.codeHarbor.tool.redis.RedisService;
+import com.example.codeHarbor.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +17,20 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class GroupCrudService {
+    private final UserRepository userRepo;
     private final GroupRepository groupRepo;
+    private final UserGroupRepository userGroupRepo;
+    private final JavaMailService mailSender;
+    private final RedisService redisService;
+    private final String ACCEPT_NEW_URL = "127.0.0.1:8081/GroupCrud/verifyAcceptNew";
+    private final String ACCEPT_EXIST_URL = "127.0.0.1:8081/GroupCrud/verifyAcceptExist";
+    private final String REDIRECT_URL = "127.0.0.1:5173/GroupCrud/signUp";
+
     public GroupCrudResponseDto createGroup(GroupCrudRequestDto input) {
         GroupCrudResponseDto response;
         Map<String, Object> data = new HashMap<>();
         try {
-            if(!groupRepo.existsByGroupName(input.getGroupName())) {
+            if (!groupRepo.existsByGroupName(input.getGroupName())) {
                 GroupDomain newGroup = new GroupDomain();
                 newGroup.setGroupCreator(input.getGroupCreator());
                 newGroup.setGroupName(input.getGroupName());
@@ -37,18 +49,60 @@ public class GroupCrudService {
         return response;
     }
 
-    public GroupCrudResponseDto inviteMemeber(GroupCrudRequestDto input) {
+    public GroupCrudResponseDto inviteMember(GroupCrudRequestDto input) {
         GroupCrudResponseDto response;
         GroupDomain currentGroup;
         Map<String, Object> data = new HashMap<>();
         try {
-            if () {
-
+            currentGroup = groupRepo.findGroupByGroupName(input.getGroupName());
+            if (!userGroupRepo.existsByUserAndGroup(userRepo.findUserByUserId(input.getGroupInvitee()), currentGroup)) {
+                if (userRepo.findUserByUserId(input.getGroupInvitee()) != null) {
+                    String verifyValue = redisService.generateCode("temp");
+                    // 초대 메일 보내는 로직
+                    response = mailSender.sendGroupInvitaiondMail(input.getGroupInvitee(), input.getGroupName(), ACCEPT_EXIST_URL, verifyValue);
+                } else {
+                    // 가입 메일 보내는 로직 + 가입 후 자동으로 그룹에 포함시키는 로직(추가 예정)
+                    response = mailSender.sendSignUpMail(input.getGroupInvitee(), input.getGroupName(), REDIRECT_URL);
+                }
+            } else {
+                data.put("msg", "이미 초대된 그룹원입니다.");
+                response = GroupCrudResponseDto.builder().success(false).data(data).build();
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+            data.put("msg", "그룹 초대과정에서 문제가 발생했습니다. 지속시 관리자에게 문의해주세요");
+            response = GroupCrudResponseDto.builder().success(false).data(data).build();
         }
+
+        return response;
     }
 
+    public GroupCrudResponseDto acceptExistUserAsMember(GroupCrudRequestDto input) {
+        GroupCrudResponseDto response;
+        Map<String, Object> data = new HashMap<>();
+        try {
+            if (redisService.retrieveDataFromRedis(input.getGroupInvitee()) == input.getGroupInviteVerify()) {
+                userGroupRepo.save(userRepo.findUserByUserId(input.getGroupInvitee()), groupRepo.findGroupByGroupName(input.getGroupName()));
+                data.put("msg", "회원을 그룹에 초대했습니다.");
+                response = GroupCrudResponseDto.builder().success(true).data(data).build();
+            } else {
+                data.put("msg", "정상적이지 않은 수락 요청입니다.");
+                response = GroupCrudResponseDto.builder().success(false).data(data).build();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            data.put("msg", "그룹 초대과정에서 문제가 발생했습니다. 지속시 관리자에게 문의해주세요");
+            response = GroupCrudResponseDto.builder().success(false).data(data).build();
+        }
+        return response;
+    }
+
+//    public GroupCrudResponseDto acceptNewUserAsMember(GroupCrudRequestDto input) {
+//        GroupCrudResponseDto response;
+//        Map<String, Object> data = new HashMap<>();
+//        // 가입신청 메일에 미리 parameter를 던져줘서 그거 기준으로 가입시키기
+//        return response;
+//    }
 }
