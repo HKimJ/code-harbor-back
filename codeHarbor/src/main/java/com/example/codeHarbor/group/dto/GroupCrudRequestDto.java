a784d1ee93ce045a68c73191811a456838664d40
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 16:58:58 +0900

    리펙터링
    request시 요청하는 필드목록 추가

--- codeHarbor/src/main/java/com/example/codeHarbor/group/dto/GroupCrudRequestDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/dto/GroupCrudRequestDto.java
@@ -6,12 +6,13 @@ import lombok.*;
 @Getter
 @ToString
 @Builder
-
 public class GroupCrudRequestDto {
     @Schema(description = "최초 그룹생성 요청을 보낸 이용자 id", example = "test@naver.com")
     private final String groupCreator;
     @Schema(description = "그룹생성 요청시 전송한 그룹이름", example = "myGroup1")
     private final String groupName;
+    @Schema(description = "그룹생성시 그룹이름 중복 체크했는지 여부", example = "true/false")
+    private final boolean isChecked;
     @Schema(description = "그룹원 초대 요청을 보낸 이용자 id", example = "invitor@naver.com")
     private final String groupInvitor;
     @Schema(description = "그룹에 초대할 인원의 userId or email 주소)", example = "invitee@naver.com")
