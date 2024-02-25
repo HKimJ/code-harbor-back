1d0d65bd1c49e5c0f0cf41481052df99d656c0d2
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 17:03:07 +0900

    리펙터링
    Id 타입에 맞는 형식으로 지정

--- codeHarbor/src/main/java/com/example/codeHarbor/user/repository/UserRepository.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/repository/UserRepository.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 @Repository
-public interface UserRepository extends JpaRepository<UserDomain, Long> {
+public interface UserRepository extends JpaRepository<UserDomain, String> {
 //    UserDomain findUserByUserSeq(Long userSeq); 필요시 추가
     UserDomain findUserByUserId(String userId);
     UserDomain findUserByUserNickname(String userNickname);
     boolean existsByUserId(String userId);
 
     boolean existsByUserNickname(String userNickname);
+
+    boolean existsAllByUserIdAndHasNewMsg(String userId, boolean hasNewMsg);
     UserDomain findUserByUserIdAndUserPassword(String userId, String userPassword);
     UserDomain save(UserDomain userDomain);
 }
