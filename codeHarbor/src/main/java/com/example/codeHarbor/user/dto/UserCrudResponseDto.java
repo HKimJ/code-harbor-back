c3b92a42bb1c735e8b6fa6c13e17f3dd79f3e124
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 17:04:43 +0900

    리펙터링

--- codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudResponseDto.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/dto/UserCrudResponseDto.java
@@ -1,6 +1,9 @@
 package com.example.codeHarbor.user.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,7 +12,7 @@ import java.util.Map;
 @Data
 public class UserCrudResponseDto {
     @Schema(description="요청 성공여부", example = "true")
-    @NotNull
+    @NotBlank
     private boolean success;
     @Schema(description = "클라이언트 요청에 따른 유저 정보를 key, value로 유저 데이터 전송<br>" +
             "로그인 실패시 실패 메시지 전달",
@@ -17,5 +20,6 @@ public class UserCrudResponseDto {
                     "data.userNickname: test " +
                     "data.userGroupName : myGroup" +
                     "data.msg: 아이디나 비밀번호가 일치하지 않습니다.")
+
     private Map<String, Object> data;
 }
