1782b430213d64139f14424d7215a87d4144ec48
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 17:09:47 +0900

    리펙터링
    그룹 초대 기능의 로직 변경에 따른 수정

--- codeHarbor/src/main/java/com/example/codeHarbor/tool/javamail/JavaMailService.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/tool/javamail/JavaMailService.java
@@ -99,28 +99,21 @@ public class JavaMailService {
         }
         return response;
     }
-    public GroupCrudResponseDto sendGroupInvitaiondMail(String email, String groupName, String acceptUrl, String verifyValue) {
+    public GroupCrudResponseDto sendGroupInvitaiondMail(String email, String groupName, String redirectUrl) {
         GroupCrudResponseDto response;
         Map<String, Object> data = new HashMap<>();
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         String title = "[CodeHarbor 그룹 초대]";
         StringBuilder content = new StringBuilder();
         content.append("<h1> [ ").append(groupName).append(" ] 그룹에서 사용자를 초대하려고 합니다.</h1>")
-                .append("<h2> 하단의 링크를 눌러 초대를 수락해주세요. </h2>")
-                .append("<form action=\"").append(acceptUrl).append("\" method=\"post\">")
-                .append("<input type=\"hidden\" name=\"groupInvitee\" value=\"").append(email).append("\">")
-                .append("<input type=\"hidden\" name=\"groupName\" value=\"").append(groupName).append("\">")
-                .append("<input type=\"hidden\" name=\"groupInviteVerify\" value=\"").append(verifyValue).append("\">")
-                .append("<button type=\"submit\">Accept</button>")
-                .append("</form>");
+                .append("<h2> 하단의 링크를 통해 CodeHarbor로 이동하고 그룹가입 요청을 수락해주세요. </h2>")
+                .append("<a href=\"").append(redirectUrl).append("\"/>");
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
             mimeMessageHelper.setTo(email); // 메일 수신자
             mimeMessageHelper.setSubject(title); // 메일 제목
             mimeMessageHelper.setText(content.toString(), true); // 메일 본문 내용, HTML 여부
             mailSender.send(mimeMessage);
-            // 이 부분은 레디스가 유일한 key값을 가져야하므로 key값을 파싱할 수 있는 구분자를 붙여서 넣는방식으로 api간 키값 중복을 방지해야함 -> 추후 리펙터링
-            redisService.storeDataInRedis(email, verifyValue, EXPIRE_MIN);
             data.put("msg", "그룹 초대메일이 발송되었습니다.");
             response = GroupCrudResponseDto.builder().success(true).data(data).build();
         } catch (Exception e) {
@@ -130,6 +123,39 @@ public class JavaMailService {
         }
         return response;
     }
+
+//  수락 메일을 보내는 로직(cors 등 해결법 찾으면 채택 고려)
+//    public GroupCrudResponseDto sendGroupInvitaiondMail(String email, String groupName, String acceptUrl, String verifyValue) {
+//        GroupCrudResponseDto response;
+//        Map<String, Object> data = new HashMap<>();
+//        MimeMessage mimeMessage = mailSender.createMimeMessage();
+//        String title = "[CodeHarbor 그룹 초대]";
+//        StringBuilder content = new StringBuilder();
+//        content.append("<h1> [ ").append(groupName).append(" ] 그룹에서 사용자를 초대하려고 합니다.</h1>")
+//                .append("<h2> 하단의 링크를 눌러 초대를 수락해주세요. </h2>")
+//                .append("<form action=\"").append(acceptUrl).append("\" method=\"post\">")
+//                .append("<input type=\"hidden\" name=\"groupInvitee\" value=\"").append(email).append("\">")
+//                .append("<input type=\"hidden\" name=\"groupName\" value=\"").append(groupName).append("\">")
+//                .append("<input type=\"hidden\" name=\"groupInviteVerify\" value=\"").append(verifyValue).append("\">")
+//                .append("<button type=\"submit\">Accept</button>")
+//                .append("</form>");
+//        try {
+//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+//            mimeMessageHelper.setTo(email); // 메일 수신자
+//            mimeMessageHelper.setSubject(title); // 메일 제목
+//            mimeMessageHelper.setText(content.toString(), true); // 메일 본문 내용, HTML 여부
+//            mailSender.send(mimeMessage);
+//            // 이 부분은 레디스가 유일한 key값을 가져야하므로 key값을 파싱할 수 있는 구분자를 붙여서 넣는방식으로 api간 키값 중복을 방지해야함 -> 추후 리펙터링
+//            redisService.storeDataInRedis(email, verifyValue, EXPIRE_MIN);
+//            data.put("msg", "그룹 초대메일이 발송되었습니다.");
+//            response = GroupCrudResponseDto.builder().success(true).data(data).build();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            data.put("msg", "그룹 초대메일 발송에 실패했습니다. 문제 지속시 관리자에게 연락해주세요.");
+//            response = GroupCrudResponseDto.builder().success(false).data(data).build();
+//        }
+//        return response;
+//    }
 
     public GroupCrudResponseDto sendSignUpMail(String email, String groupName, String redirectUrl) {
         GroupCrudResponseDto response;
