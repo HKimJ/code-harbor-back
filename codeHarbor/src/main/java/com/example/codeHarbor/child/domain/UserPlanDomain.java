5a03cde46c9ed1b5f6a63f4f7c9fec94a4d28b2b
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 17:12:45 +0900

    기능추가
    계획과 유저에 관련된 중간엔티티 추가

---
+++ codeHarbor/src/main/java/com/example/codeHarbor/child/domain/UserPlanDomain.java
@@ -0,0 +1,16 @@
+//package com.example.codeHarbor.child.domain;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class UserPlanDomain {
+////    private String
+//}
