89cb2d853b771f9f990d32c3727fe0946085d91b
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 16:58:35 +0900

    리펙터링
    equals, hashcode 오버라이딩

--- codeHarbor/src/main/java/com/example/codeHarbor/group/domain/GroupDomain.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/group/domain/GroupDomain.java
@@ -1,11 +1,11 @@
 package com.example.codeHarbor.group.domain;
 
+import com.example.codeHarbor.child.domain.UserGroupDomain;
+import com.example.codeHarbor.user.domain.UserMessageDomain;
 import jakarta.persistence.*;
 import lombok.*;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+
+import java.util.*;
 
 @Entity
 @Data
@@ -13,26 +13,30 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupDomain {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long groupSeq;
-    @Column(columnDefinition = "VARCHAR(50)", updatable = false)
-    private String groupCreator;
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer groupId;
+    @Id
     @Column(columnDefinition = "VARCHAR(30)", unique = true)
     private String groupName;
+    @Column(columnDefinition = "VARCHAR(50)", updatable = false)
+    private String groupCreator;
     @Temporal(value = TemporalType.DATE) @Column(columnDefinition = "DATE DEFAULT (CURRENT_DATE)", insertable = false, updatable = false)
     private Date groupCreatedate;
+
+    @OneToMany(mappedBy = "joinedGroup")
+    private List<UserGroupDomain> groupMembers;
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if(obj == null || getClass() != obj.getClass()) return false;
         GroupDomain group = (GroupDomain) obj;
-        return Objects.equals(groupSeq, group.groupSeq);
+        return Objects.equals(groupName, group.groupName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(groupSeq);
+        return Objects.hash(groupName);
     }
 
 }
