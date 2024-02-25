2cb8fd16e421eeb2adc8e5d3cad59edf474c6888
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-25 16:55:50 +0900

    리펙터링
    유저와 그룹을 외래키로 연결하지 않는 방식으로 변경

--- codeHarbor/src/main/java/com/example/codeHarbor/child/domain/UserGroupDomain.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/child/domain/UserGroupDomain.java
@@ -3,20 +3,27 @@ package com.example.codeHarbor.child.domain;
 import com.example.codeHarbor.group.domain.GroupDomain;
 import com.example.codeHarbor.user.domain.UserDomain;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Entity
 @Data
 @Table(name="Group_Members")
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserGroupDomain {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long seq;
-    @ManyToOne @JoinColumn(name="user_Id", referencedColumnName = "userId")
+    private Long userGroupId;
+    @ManyToOne
     private UserDomain user;
-    @ManyToOne @JoinColumn(name="group_Name", referencedColumnName = "groupName")
-    private GroupDomain group;
+    @ManyToOne
+    private GroupDomain joinedGroup;
+    @Column(columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean accepted;
+    @Temporal(value = TemporalType.DATE) @Column(columnDefinition = "DATE DEFAULT (CURRENT_DATE)", insertable = false) // 이러면 임시로 가입된 정보를 넣은 시점이 저장되어버림, 수락 시점에 쿼리를 한번 더 날려야할듯
+    private Date userGroupJoinDate;
+
 }
