9879dd9820b82a86c60122856b854671a5ec1682
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-28 19:28:18 +0900

    단순수정

--- codeHarbor/src/main/java/com/example/codeHarbor/user/repository/UserMessageRepository.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/repository/UserMessageRepository.java
@@ -3,13 +3,15 @@ package com.example.codeHarbor.user.repository;
 import com.example.codeHarbor.user.domain.MessageDomain;
 import com.example.codeHarbor.user.domain.UserDomain;
 import com.example.codeHarbor.user.domain.UserMessageDomain;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserMessageRepository extends JpaRepository<UserMessageDomain, Long> {
-    boolean existsAllByMessageOwnerAndIsRead(UserDomain messageOwner, boolean isRead); // 유저가 읽지 않은 메시지가 있는지
+    boolean existsAllByMessageOwnerAndIsRead(UserDomain messageOwner, boolean isRead); // 유저가 읽지 않은 메시지가 있는지 체크
     List<UserMessageDomain> findAllByMessageOwnerAndIsRead(UserDomain messageOwner, boolean isRead); // 유저가 읽지 않은 메시지 목록 전체 반환
+    UserMessageDomain findByMessageOwnerAndIsRead(UserDomain messageOwner, boolean isRead);
 
     boolean deleteByMessageOwner(UserDomain messageOwner); // 유저가 보유한 전체 메세지 비우기
     boolean deleteAllByMessageOwnerAndIsRead(UserDomain messageOwner, boolean isRead); // 읽은 메세지 삭제
