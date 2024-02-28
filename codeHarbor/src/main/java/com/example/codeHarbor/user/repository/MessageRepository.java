e51d3259f256b8d26ffa916bf9cced891186c61c
HKimJ <72964843+HKimJ@users.noreply.github.com>
2024-02-28 19:27:59 +0900

    리펙터링
    로직수정으로 인해 필요해진 메서드 형태로 변경

--- codeHarbor/src/main/java/com/example/codeHarbor/user/repository/MessageRepository.java
+++ codeHarbor/src/main/java/com/example/codeHarbor/user/repository/MessageRepository.java
@@ -2,11 +2,10 @@ package com.example.codeHarbor.user.repository;
 
 import com.example.codeHarbor.user.domain.MessageDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface MessageRepository extends JpaRepository<MessageDomain, Long> {
     MessageDomain findByMsgContent(String msgContent);
-    MessageDomain findByMsgContentContainingAndMsgContentContainingAndMsgContentContaining(String userId, String groupName, String msgType);
+    MessageDomain findByMsgContentStartsWithAndMsgContentContainsAndMsgContentEndsWith(String groupInvitor, String groupInvitee, String inviteMsg);
     boolean deleteByMsgId(Long msgId);
     MessageDomain save(MessageDomain message);
 }
